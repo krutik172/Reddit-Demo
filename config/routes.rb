470f74d6b95dd2be048ef408a8a108b729644fa6
Krutik Thaker <kthaker17@gmail.com>
2021-07-22 16:53:18 +0530

    Tumblr app crud operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
- resources :posts
- root 'posts#index'
+  
+  root 'posts#index'
+  
+  resources :posts do
+  resources :comments
+  
+ end
+ get '/about', to: 'pages#about'
 end
