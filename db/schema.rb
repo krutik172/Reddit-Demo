1ab5a4b0f47607aa616b7f85cecba85dce1b659e
Krutik Thaker <kthaker17@gmail.com>
2022-01-10 18:09:13 +0530

    Changed DB to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_10_100007) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "commentor"
     t.text "body"
